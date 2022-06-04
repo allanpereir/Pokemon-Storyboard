facfd9de9bb0b11c1ac65971eae287478a87ddf4
Alan <allanpereir@gmail.com>
2022-06-04 12:35:38 -0300

    Comite Inicial.

---
+++ Pokemon/Controller/PokemonListCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonListCell.swift
+//  Pokemon
+//
+//  Created by Alan Silva on 04/06/22.
+//
+
+import UIKit
+
+class PokemonListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Pokemon/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Alan Silva on 04/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
